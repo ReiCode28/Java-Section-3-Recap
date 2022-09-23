4792ec8d1253845aa014d48531e3f0d11e0716ea
randy <randyr_1993@yahoo.com>
2022-09-23 19:47:09 -0400

    Completed Quiz App

--- Quiz.java
+++ Quiz.java
@@ -8,38 +8,61 @@ public class Quiz {
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
         String country = scan.nextLine();
-        //store answer 1
 
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
         String planet = scan.nextLine();
-        //store answer 2
 
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
         String bloodType = scan.nextLine();
-        //store answer 3
 
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
-        String characters = scan.nextLine();
-        //store answer 4
+        String character = scan.nextLine();
 
         int score = 0;
 
+        if (country.equals("c")){
+            score += 5;
+        }
 
-        //Task 2: Check each answer - For each correct answer, add 5 points to the score. 
+        if (planet.equals("a")){
+            score += 5;
+        }
+
+        if (bloodType.equals("d")){
+            score += 5;
+        }
+
+        if (character.equals("a") || character.equals("b")){
+            score += 5;
+        }
         
         System.out.println("Your final score is: " + score + "/20");
 
-        //Task 3: print a message depending on the score
-
-        // if the score is 15 or higher, print: "Wow, you know your stuff!";
-        // if the score is between 5 and 15, print "Not bad!";
-        // else, better luck next time. 
-
+        if (score >= 15){
+            System.out.println("Wow, you know your stuff!");
+        } else if (score < 15 && score >=5){
+            System.out.println("Not bad!");
+        } else {
+            System.out.println("better luck next time.");
+        }
 
         scan.close();
 
     }
 }
+
+/*
+A quiz that asks multiple-choice questions about common trivia.
+It tracks the user's answers and prints their final score.
+
+Correct answers to the questions are as follows:
+
+Question 1: b
+Question 2: c
+Question 3: d
+Question 4: c
+20/20
+ */ 
