baccbacfd67e89f211bbfe9ef60506328da21311
randy <randyr_1993@yahoo.com>
2022-09-23 15:34:38 -0400

    Completed Calendar

--- Calendar.java
+++ Calendar.java
@@ -1,12 +1,24 @@
+//Switch Statements Practice
 public class Calendar {
     public static void main(String[] args) {
-        String day = "Friday"; //Can be any day.
-        System.out.println("Hey, are you free on " + day + "? \n");
+        String day = "Friday"; 
+        System.out.println("\nFriend: Hey, are you free on " + day + "? \n");
 
 
-        System.out.println("Hmm, let me check my calendar.");
+        System.out.println("You: Hmm, let me check my calendar.");
+
+        switch (day){
+            case "Monday": System.out.println("Sorry, I have to stay at work late.\n"); break;
+            case "Tuesday": System.out.println("It looks like I have meetings all day.\n"); break;
+            case "Wednesday": System.out.println("I have a dentist appointment. Some other time!\n"); break;
+            case "Thursday": System.out.println("Sorry, thursday is date night!\n"); break;
+            case "Friday": System.out.println("I'm free!!\n"); break;
+            case "Saturday": System.out.println("I'm free!!\n"); break;
+            case "Sunday": System.out.println("I'm free!!\n"); break;
+            default : System.out.println("that's not a day.\n"); break;
+        }
         
-        //Check calendar here:
+        //Checks calendar to see if you are free
 
             //  "Monday": print ("Sorry, I have to stay at work late."); 
             //  "Tuesday": print ("It looks like I have meetings all day."); 
@@ -17,8 +29,6 @@ public class Calendar {
             //  "Sunday": print ("I'm free!!"); 
             //  "otherwise": print ("that's not a day."); 
 
-        
-        
 
     }
 }
