1e844dc9b73cc51343a74c36a6f05568a8bb5799
randy <randyr_1993@yahoo.com>
2022-09-23 18:35:17 -0400

    Completed Bank App

--- Bank.java
+++ Bank.java
@@ -5,29 +5,41 @@ public class Bank {
         Scanner scan = new Scanner(System.in);
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
-        //Task 1 - Pick up the user's decision.
+        String decision = scan.nextLine();
 
-
-        //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
+        if (decision.equals("yes")) {
+            System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
+            int savings = scan.nextInt();
+            int creditDebt = scan.nextInt();
+
             System.out.println("\nHow many years have you worked for?");
-               // Task 4 - Pick up number of years
+            int workYears = scan.nextInt();
 
             System.out.println("What is your name?");
-               // Task 5 - Pick up the user's name
-                
-               //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
+            String name = scan.next();
 
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
+            if (savings >= 10000 && creditDebt <= 5000 && workYears >= 2) {
+                System.out.println("Congratulations " + name + ", you have been approved!");
+            } else {
+                System.out.println("Sorry, you are not eligible for a mortage");
+            }
 
+        } else if (decision.equals("no")) {
+            System.out.println("Okay, have a nice day!");
+        } else if (!decision.equals("yes") || !decision.equals("no")) {
+            System.out.println("invalid option");
+        }
 
         scan.close();
     }
 }
+
+/*
+ * Approves customers for a loan if they meet the following requirements:
+ * Have $10,000 in their savings account
+ * Have less than $5,000 in credit card debt
+ * Have worked for more than 2 years
+ * Reicode28
+ */
